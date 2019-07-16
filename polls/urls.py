4ead26913444603c8dae93d0ecf818c0d879f544
Matthew Brandeburg <matthewbrandeburg@datamachines.io>
2019-07-16 11:34:14 -0700

    got results and votes pages up

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),  
     # localhost:8000/polls
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # localhost:8000/polls/#
+    url(r'^(?P<question_id>[0-9]+)/results$', views.results, name='results'),
+    # localhost:8000/polls/#/results
+    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name='vote'),
+    # localhost:8000/polls/#/vote
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Awesome job guys! This is the index page of our polls application.')
+
+
+def detail(request, question_id):
+    return HttpResponse("This is the detailed view of the question: {}".format(question_id))
+
+def results(request, question_id):
+    return HttpResponse("These are the results of the question: {}".format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("Votes for question: {}".format(question_id))
