5696b2c6c3389331a07b209549ab36760d0a8e66
Matthew Brandeburg <matthewbrandeburg@datamachines.io>
2019-07-16 11:24:39 -0700

    miurls and views basics up and running for routing in the app

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include #b/c did like instructions above
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
 ]
