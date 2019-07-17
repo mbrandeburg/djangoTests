02e7c62b11482ffcde64f3d3390c5932520721f6
Matthew Brandeburg <matthewbrandeburg@datamachines.io>
2019-07-17 11:58:57 -0700

    revised rendering for simplificacation of index

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
-from django.template import loader, RequestContext
+# from django.template import loader, RequestContext # no longer needed after `revised - simplified` under index
+
 # Create your views here.
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
+    ## original way:
     # output = ", ".join(q.question_text for q in latest_questions)
     # return HttpResponse(output)
+    
     #### revised:
-    # in {}: passing a dictionary here of what we are passing, like leaderboard into jinja variables in index.html
-    context = RequestContext(request, {
-        'latest_questions':latest_questions
-        })
-    context_dict = context.flatten() #turn it into the dictionary
-    template = loader.get_template('polls/index.html')
-    return HttpResponse(template.render(context_dict))
+    ## in {}: passing a dictionary here of what we are passing, like leaderboard into jinja variables in index.html
+    # context = RequestContext(request, {
+    #     'latest_questions':latest_questions
+    #     })
+    # context_dict = context.flatten() #turn it into the dictionary
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context_dict))
+
+    #### revised - simplified:
+    context = {'latest_questions':latest_questions}
+    return render(request,'polls/index.html', context)
+
 
 def detail(request, question_id):
     return HttpResponse("This is the detailed view of the question: {}".format(question_id))
