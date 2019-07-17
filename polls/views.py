266cfac0bb189e01d4a25481d1dfa065b37a9505
Matthew Brandeburg <matthewbrandeburg@datamachines.io>
2019-07-16 18:10:10 -0700

    for loops added

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader, RequestContext
 # Create your views here.
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join(q.question_text for q in latest_questions)
-    return HttpResponse(output)
-
+    # output = ", ".join(q.question_text for q in latest_questions)
+    # return HttpResponse(output)
+    #### revised:
+    # in {}: passing a dictionary here of what we are passing, like leaderboard into jinja variables in index.html
+    context = RequestContext(request, {
+        'latest_questions':latest_questions
+        })
+    context_dict = context.flatten() #turn it into the dictionary
+    template = loader.get_template('polls/index.html')
+    return HttpResponse(template.render(context_dict))
 
 def detail(request, question_id):
     return HttpResponse("This is the detailed view of the question: {}".format(question_id))
